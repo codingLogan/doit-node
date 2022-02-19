4135ec27878161a2f1d4edb7b74897ed62ac7532
codingLogan <reachCodingLogan@gmail.com>
2022-02-18 21:14:47 -0700

    Export using ESModule syntax

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-exports.printMsg = function () {
+function printMsg() {
   console.log("This is a message from the demo package");
-};
+}
+
+import { ToDoList } from "./entities/ToDoList";
+
+// Public API
+export { printMsg, ToDoList };
