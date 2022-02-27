73717b61865969a6a6ead1780e6d6fcc62ab564f
codingLogan <reachCodingLogan@gmail.com>
2022-02-27 13:31:26 -0700

    Export new use case to complete list items

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { CreateListInteractor } from "./use_cases/CreateList";
 import { GetListsInteractor } from "./use_cases/GetLists";
 import { AddItemInteractor } from "./use_cases/AddItem";
+import { CompleteItemInteractor } from "./use_cases/CompleteItem";
 
 import { TestRepository } from "./repositories/repository";
 
@@ -9,5 +10,6 @@ export {
   CreateListInteractor,
   GetListsInteractor,
   AddItemInteractor,
+  CompleteItemInteractor,
   TestRepository,
 };
