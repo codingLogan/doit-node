f91ed2e2ac0d47f8800bdfae3b01c1cfd32590d1
codingLogan <reachCodingLogan@gmail.com>
2022-02-27 09:25:49 -0700

    WIP - adding test for marking items as done
    Next step is to create the strategy that relates the entities together more concretely

--- src/__tests__/use_cases/mark_item_done.test.ts
+++ src/__tests__/use_cases/mark_item_done.test.ts
@@ -1,7 +1,25 @@
-import { ToDoItem } from "../../entities/ToDoItem";
+import { ToDoItem, ToDoItemInterface } from "../../entities/ToDoItem";
 
 test("Mark item as done", () => {
   const toDoItem = new ToDoItem("chores");
   toDoItem.markAsDone();
   expect(toDoItem.isDone()).toBe(true);
 });
+
+// test("Mark item as done in repository", () => {
+//   const input = {
+//     listId: "1",
+//     itemId: "1",
+//   };
+
+//   const output: ToDoItemInterface = {
+//     name: "testname",
+//     done: true,
+//     id: "1",
+//   };
+
+//   const interactor = new MarkItemAsDoneInteractor(repo);
+//   const actual = interactor.markItemAsDone(input);
+
+//   expect(actual).toStrictEqual(output);
+// });
