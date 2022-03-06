49f3cfba552cdb694ce6b92e9407f3925c90b909
codingLogan <reachCodingLogan@gmail.com>
2022-03-06 10:52:23 -0700

    Export all interactors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
+// List Use Cases
 import { CreateListInteractor } from "./use_cases/CreateList";
 import { GetListsInteractor } from "./use_cases/GetLists";
+import { DeleteListInteractor } from "./use_cases/DeleteList";
+
+// Item Use Cases
 import { AddItemInteractor } from "./use_cases/AddItem";
 import { CompleteItemInteractor } from "./use_cases/CompleteItem";
+import { DeleteItemInteractor } from "./use_cases/DeleteItem";
 
+// Database plugins
 import { TestRepository } from "./repositories/repository";
 
 // Public API
 export {
   CreateListInteractor,
   GetListsInteractor,
+  DeleteListInteractor,
   AddItemInteractor,
   CompleteItemInteractor,
+  DeleteItemInteractor,
   TestRepository,
 };
